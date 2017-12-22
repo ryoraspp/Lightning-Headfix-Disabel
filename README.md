f3f88e5f26e3b8ea8fbd7586e213dece93476939
ryoraspp <ryoraspp@gmail.com>
2017-12-22 22:15:31 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# lightning-headfix-disabel
+
+Lightning 専用のプラグイン
+
+ヘッダーの固定を解除
---
+++ lightning-headfix-disabel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Lightning Headfix Disabel
+ * Version: 1
+ * Author: ryoraspp
+ * Author URI: https:/ryo.nagoya
+ * Description: This is a plug-ins that  disable the headlix of the theme "Lightning".
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+add_filter( 'lightning_headfix_enable', 'lightning_headfix_disabel');
+
+function lightning_headfix_disabel(){
+    return false;
+}
